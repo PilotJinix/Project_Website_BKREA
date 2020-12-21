07138231835fbe63e447569b4e734ac42ae760f3
PilotJinix <agungsantoso557@gmail.com>
2020-12-22 02:43:00 +0700

    up

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,19 @@
+<meta charset="UTF-8" />
+<title>@yield('title') - WG Farm</title>
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta name="theme-color" content="#ffffff">
+<meta name="msapplication-TileColor" content="#000000">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<base #href=""/>
+<link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700;900&amp;display=swap" rel="stylesheet">
+<link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="{{asset('assets/images/favicon/favicon-32x32.png')}}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/images/favicon/favicon-16x16.png')}}">
+<link rel="stylesheet" href="{{asset('assets/css/plugins.min.css')}}" />
+<link rel="stylesheet" href="{{asset('assets/css/styles.css')}}" />
+<link rel="stylesheet" href="{{asset('assets/css/skin-1.css')}}" />
+<link rel="stylesheet" href="{{asset('assets/css/custom.css')}}" />
+<link rel="stylesheet" href="{{asset('assets/css/demo.css')}}" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
